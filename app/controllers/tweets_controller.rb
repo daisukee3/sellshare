713031939e5df1e6e557e665d663a927167597a1
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-19 22:16:09 +0900

    createアクションを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,4 +12,19 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path(@tweet)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
