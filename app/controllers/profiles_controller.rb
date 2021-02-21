5a16c1c252b549e4dad6c1f75c2cfd056dd4a4e6
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-21 20:21:03 +0900

    updateアクション#22

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,4 +9,19 @@ class ProfilesController < ApplicationController
     @profile = current_user.build_profile
   end
 
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: '更新完了'
+    else
+      flash.now[:error] = '更新失敗'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:introduction)
+  end
+
 end
