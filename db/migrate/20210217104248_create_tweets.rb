51c82df8f6bc169831e10b0bec78ec6b3fa2020d
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-21 14:07:44 +0900

    tweetとuserのDB修正

--- db/migrate/20210217104248_create_tweets.rb
+++ db/migrate/20210217104248_create_tweets.rb
@@ -1,7 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.text :content
+      t.references :user, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
