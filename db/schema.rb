d1e23f8c4f96828adab16e463c71a6c1d88c341d
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-21 14:39:56 +0900

    commentモデルを作成#21

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_104248) do
+ActiveRecord::Schema.define(version: 2021_02_21_053311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "tweet_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.bigint "user_id", null: false
