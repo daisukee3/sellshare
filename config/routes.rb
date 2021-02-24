dace23daf9244d3af55b4383fcf8ac44e5f3fe42
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-24 16:31:36 +0900

    タイムラインテンプレート

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
 
+  resource :timeline, only: [:show]
+
   resources :tweets do
     resources :comments, only: [:new, :create]
 
