522b0435d18e2acaf67fba55c1d13b78937fb195
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-24 18:10:14 +0900

    activedecorator導入し変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -22,11 +22,4 @@ class Tweet < ApplicationRecord
   has_one_attached :eyecatch
   has_rich_text :content
 
-  def display_created_at
-    I18n.l(self.created_at, format: :default)
-  end
-
-  def like_count
-    likes.count
-  end
 end
