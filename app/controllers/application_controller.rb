6fd437284dead1bc078a20df10f367566a6598a1
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-26 01:39:22 +0900

    多言語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
 
@@ -16,4 +17,12 @@ class ApplicationController < ActionController::Base
     super
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
