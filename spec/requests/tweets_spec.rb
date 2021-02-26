61a8e8ccafa03b0805c06894abe4f3c43cc962a0
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-27 01:53:52 +0900

    tweetsのindexのテスト追加

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Tweets', type: :request do
+  let!(:user) { create(:user) }
+  let!(:tweet) { create_list(:tweet, 5,  user: user) }
+
   describe 'GET /tweets' do
-    it "200statusが返ってくる" do
+    it '200statusが返ってくる' do
       get tweets_path
       expect(response).to have_http_status(200)
     end
