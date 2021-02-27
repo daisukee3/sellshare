455e671e65834db98a8620e9d1a855f6e9a576f1
daisuke_nabatame <nabanaba1225@gmail.com>
2021-02-27 20:25:59 +0900

    プロフィールのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     account { Faker::Lorem.characters(number: 5) }
     email { Faker::Internet.email }
     password { 'password' }
+
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
+    end
   end
 end
