aa9b9dd9f735af26df7f5f92c59ef63e6bc3dcca
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-01 15:10:12 +0900

    age等の表示を登録状況によって分ける

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -24,4 +24,28 @@ class Profile < ApplicationRecord
   enum gender: { male: 0, female: 1, other: 2 }
   enum age: { teens: 0, twenties: 1, thirties: 2, forties: 3, fifties: 4, sixtiesover: 5 }
   enum type: { maker: 0, trade: 1, it: 2, ad: 3, human: 4, realestate: 5, consulting: 6, dealer: 7, insurance: 8, finance: 9, typeother: 10 }
+
+  def user_gender
+    if profile&.gender&.attached?
+      profile.gender
+    else
+      ''
+    end
+  end
+
+  def user_age
+    if profile&.age&.attached?
+      profile.age
+    else
+      ''
+    end
+  end
+
+  def user_type
+    if profile&.type&.attached?
+      profile.type
+    else
+      ''
+    end
+  end
 end
