c566a4a9db627ab005d7eaf09f37499f67c8e240
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-06 16:04:34 +0900

    ランキング上位5つを表示#39

--- app/controllers/apps/popular_posts_controller.rb
+++ app/controllers/apps/popular_posts_controller.rb
@@ -1,7 +1,7 @@
 class Apps::PopularPostsController < Apps::ApplicationController
   def show
-    
-    tweet_like_count = Tweet.joins(:likes).group(:tweet_id).count
+    @tweets = Tweet.limit(5)
+    tweet_like_count = @tweets.joins(:likes).group(:tweet_id).count
     tweet_liked_ids = Hash[tweet_like_count.sort_by{ |_, v| v }.reverse].keys
     @tweet_rankings= Tweet.find(tweet_liked_ids)
   end
