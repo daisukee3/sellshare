63f7b956c35692b8e2eaf2e1175a8c3bf230030d
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-10 16:57:55 +0900

    コントローラー、routesを設定、indexへのリンクを作成#59

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :notifications, only: [:index]
 
-  resources :complaints, only: [:index, :create, :destroy]
+  resources :complaints, only: [:index, :new, :create, :destroy]
 
   resources :accounts, only:[:show] do
     resources :follows, only:[:index, :show, :create]
