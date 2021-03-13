c8370bbf060c0c36a9e3df3f1d4c65dac3a66614
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-14 03:06:44 +0900

    表示数を５つに制限、デザイン変更

--- app/controllers/apps/timelines_controller.rb
+++ app/controllers/apps/timelines_controller.rb
@@ -1,6 +1,6 @@
 class Apps::TimelinesController < Apps::ApplicationController
   def show
     user_ids = current_user.followings.pluck(:id)
-    @tweets = Tweet.where(user_id: user_ids)
+    @tweets = Tweet.where(user_id: user_ids).page(params[:page]).per(5)
   end
 end
