4f1f5ef6fa0a9ddc7cd43a967f4f73e55cb536b2
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-21 23:34:09 +0900

    ニュースを表示できるように設定#66

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,11 @@
+class NewsController < ApplicationController
+  def index
+  end
+
+  def data
+    uri = URI.parse("http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=#{ENV['API_KEY']}")
+    json = Net::HTTP.get(uri)
+    moments = JSON.parse(json)
+    @data = moments['articles'].to_json
+  end
+end
