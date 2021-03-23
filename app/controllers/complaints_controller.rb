b1d91b971cae01077af76ea2af2d499fcbc60aa1
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-23 18:05:24 +0900

    コントローラーを変更

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -2,29 +2,28 @@ class ComplaintsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # @complaints = Complaint.all
+    @complaint = Complaint.new
     @complaints = Complaint.where('complaints.created_at > ?', Date.today)
   end
 
   def create
     @complaint = current_user.complaints.build(complaint_params)
-    if @complaint.save
-      redirect_to complaints_path, notice: '保存完了'
-    else
-      flash.now[:error] = '保存失敗'
-      render :new
-    end
+    @complaint.user_id = current_user.id
+    @complaint.save
+      # redirect_to complaints_path, notice: '保存完了'
+    @complaints = current_user.complaints.where('complaints.created_at > ?', Date.today)
+    render :index
 
-  end
+      # flash.now[:error] = '保存失敗'
 
-  def new
-    @complaint = current_user.complaints.build
   end
 
   def destroy
     complaint = current_user.complaints.find(params[:id])
     complaint.destroy!
-    redirect_to complaints_path, notice: '削除完了'
+    @complaints = current_user.complaints.where('complaints.created_at > ?', Date.today)
+    render :index
+    # redirect_to complaints_path, notice: '削除完了'
   end
 
   private
