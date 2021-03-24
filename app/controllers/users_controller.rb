96811953ac079b1ae7378dc99a07f76fd4e7772c
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-24 16:24:02 +0900

    自分のアカウントをプロフィールから削除できるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:destroy]
   def index
     @users = User.all.page(params[:page]).per(10)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to root_path, notice: '削除完了'
   end
 
 end
