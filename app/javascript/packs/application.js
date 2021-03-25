06d3085d526847291c565a6219662d4d93ec02c3
daisuke_nabatame <nabanaba1225@gmail.com>
2021-03-25 16:48:43 +0900

    文字数カウンターを設置（tweet、コメント、ぐち)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -87,32 +87,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-//■page topボタン
+//page topボタン
 $(function(){
   var topBtn=$('#pageTop');
   topBtn.hide();
   
-  //◇ボタンの表示設定
   $(window).scroll(function(){
     if($(this).scrollTop()>80){
-  
-      //---- 画面を80pxスクロールしたら、ボタンを表示する
       topBtn.fadeIn();
-  
     }else{
-  
-      //---- 画面が80pxより上なら、ボタンを表示しない
       topBtn.fadeOut();
-  
     }
   });
   
-  // ◇ボタンをクリックしたら、スクロールして上に戻る
   topBtn.click(function(){
     $('body,html').animate({
     scrollTop: 0},500);
     return false;
-  
   });
-  
+
   });
+
+
+// 文字数カウンター
+$(function (){
+  var count = $(".js-text").text().replace(/\n/g, "改行").length;
+  var now_count = 300 - count;
+  if (count > 300) {
+    $(".js-text-count").css("color","red");
+  }
+  $(".js-text-count").text( "残り" + now_count + "文字");
+
+  $(".js-text").on("keyup", function() {
+    var count = $(this).val().replace(/\n/g, "改行").length;
+    var now_count = 300 - count;
+
+    if (count > 300) {
+      $(".js-text-count").css("color","red");
+    } else {
+      $(".js-text-count").css("color","black");
+    }
+    $(".js-text-count").text( "残り" + now_count + "文字");
+  });
+});
+
+
