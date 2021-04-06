67663738c00a02c923ec995c46b8e15700ebfc4b
daisuke_nabatame <nabanaba1225@gmail.com>
2021-04-06 17:35:36 +0900

    プロフィールのリファクタリング&テスト追加#91

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :profile do
-    introduction { Faker::Lorem.characters(number: 100) }
+    introduction { Faker::Lorem.characters(number: 300) }
     gender { Profile.genders.keys.sample }
     age { Profile.ages.keys.sample }
     type { Profile.types.keys.sample }
