5b4eef87316c641eac3557f2acaed96b800aa1d7
daisuke_nabatame <nabanaba1225@gmail.com>
2021-04-15 01:22:19 +0900

    annotateにより情報を追加

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :bigint           not null, primary key
+#  age          :integer
+#  gender       :integer
+#  introduction :text
+#  type         :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
